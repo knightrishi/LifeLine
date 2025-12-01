00af7bc865f31fee9081f98e7b98c4bf1b174501
knightrishi <arnav.ara05@gmail.com>
2025-12-02 00:07:30 +0530

    Added Hero Section

--- frontend/Frontend-Life/src/components/Hero.jsx
+++ frontend/Frontend-Life/src/components/Hero.jsx
@@ -3,17 +3,13 @@ import hero1 from "../assets/hero1.png";
 function Hero() {
   return (
     <div className="relative min-h-[60vh] flex items-center rounded-b-2xl rounded-t-2xl overflow-hidden">
-
-      {/* Full Background Image */}
       <div
         className="absolute inset-0 bg-cover bg-center"
         style={{ backgroundImage: `url(${hero1})` }}
       ></div>
 
-      {/* Dark to Transparent Overlay */}
       <div className="absolute inset-0 bg-linear-to-r from-[#0A1A2F]/95 via-[#0A1A2F]/80 to-transparent"></div>
 
-      {/* Left Content */}
       <div className="relative z-10 max-w-3xl px-12 py-20">
         <h1 className="text-white text-5xl md:text-6xl font-extrabold leading-tight">
           LIFE LINE: Bridging the Gap <br /> in Blood Management
@@ -23,7 +19,6 @@ function Hero() {
           A comprehensive platform for a healthier tomorrow.
         </p>
 
-        {/* Buttons */}
         <div className="flex gap-4 mt-8">
           <button className="bg-white text-[#0A1A2F] font-semibold px-6 py-3 rounded-md hover:bg-gray-200 transition">
             Learn More

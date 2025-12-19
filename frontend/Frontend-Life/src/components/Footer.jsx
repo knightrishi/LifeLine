1ed60f2be65c4bbd442792581527d9cb5d2e14e2
knightrishi <arnav.ara05@gmail.com>
2025-12-19 15:22:54 +0530

    Footer For Lnading page

---
+++ frontend/Frontend-Life/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="bg-[#0A1A2F]  py-0 flex justify-center items-center ">
+      <p className="text-gray-400 text-sm tracking-wide text-center">
+        © 2026 All rights reserved · Built by Arnav.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;

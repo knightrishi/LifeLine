ba9ea24a6c91f517d309f5c86a466ca324fa27da
knightrishi <arnav.ara05@gmail.com>
2025-12-19 15:22:26 +0530

    Conatct for landing Page

---
+++ frontend/Frontend-Life/src/components/Contact.jsx
@@ -0,0 +1,27 @@
+function Contact() {
+  return (
+   <section className="bg-[#0A1A2F] w-screen py-10 pb-10 mt-10 mb-0 m-h-[30vh]">
+  <div className="max-w-4xl mx-auto text-left">
+    <h2 className="text-4xl font-bold text-white">Contact</h2>
+    <p className="text-blue-200 mt-4">
+      Reach out to us. We're listening.
+    </p>
+    <p className="text-blue-200 mt-4 font-bold">
+
+      Email: lifelinehealth@gmail.com
+    </p>
+    <p className="text-blue-200 mt-4 font-bold">
+      Phone: +91-9471250494
+    </p>
+    <p className="text-blue-200 mt-4 font-bold">
+     Address: Mission Road,Pakri Ara,Bihar
+    </p>
+    
+  </div>
+  
+</section>
+
+  );
+}
+
+export default Contact;

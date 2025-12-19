3137831ad370ce59da6b00f5ed9b7486ef70d5b2
knightrishi <arnav.ara05@gmail.com>
2025-12-19 15:24:33 +0530

    Landing Page Done

--- frontend/Frontend-Life/src/App.jsx
+++ frontend/Frontend-Life/src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Features from './components/Features/Features'
+import FeaturesCard from "./components/Features/FeaturesCard"
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <Navbar />
       <Hero />
+      <Features />
+      <FeaturesCard />
+      <Contact />
+      <Footer />
     </>
   )
 }

e1e8041df2afd50de04ce40ec2e455c9bc23a567
knightrishi <arnav.ara05@gmail.com>
2025-12-30 19:33:27 +0530

    LAnding Page Added

---
+++ frontend/Frontend-Life/src/pages/Landing.jsx
@@ -0,0 +1,24 @@
+import "../App.css";
+
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Features from "../components/Features/Features";
+import FeaturesCard from "../components/Features/FeaturesCard";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+function App() {
+  
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <FeaturesCard />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App

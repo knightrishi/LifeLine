501ca7ba8136e8cae245dc3e25c9cec935af24a4
knightrishi <arnav.ara05@gmail.com>
2025-12-30 20:42:33 +0530

    Login Page

--- frontend/Frontend-Life/src/pages/LoginPage.jsx
+++ frontend/Frontend-Life/src/pages/LoginPage.jsx
@@ -1,27 +1,39 @@
-
-
+import sign from "../assets/signup.png"
+import { useNavigate } from "react-router-dom";
 function Login(){
+    const navigate=useNavigate();
     return(
-    <section className="min-h-screen flex items-center justify-center bg-cyan-50">
-      <div className="w-full max-w-sm border-2 border-blue-500 rounded-2xl bg-white p-8 space-y-6">
-        <h2 className="text-2xl font-bold text-blue-500 text-center">
+    <section className=" relative min-h-screen flex items-center justify-center bg-cyan-50">
+        <div
+                className="absolute inset-0 bg-cover bg-center"
+                style={{ backgroundImage: `url(${sign})` }}
+              ></div>
+      <div className=" relative w-full max-w-sm border-2 border-[#666868] inset-0 bg-linear-to-r from-[#0A1A2F]/95 via-[#0A1A2F]/90 to-transparent rounded-2xl b p-8 space-y-6">
+        <h2 className="text-2xl font-bold text-white text-center">
           Login
         </h2>
 
         <div className="flex flex-col space-y-1">
-          <label className="text-blue-500 font-medium">Email</label>
+          <label className="text-white font-medium">Email</label>
           <input
             type="email"
-            className="border rounded-lg px-3 py-2"
+            className="border rounded-lg px-3 py-2 text-white"
             placeholder="you@example.com"
           />
-        </div>
+
+
+
+
+
+
+          
+        </div> 332121
 
         <div className="flex flex-col space-y-1">
-          <label className="text-blue-500 font-medium">Password</label>
+          <label className="text-white font-medium">Password</label>
           <input
             type="password"
-            className="border rounded-lg px-3 py-2"
+            className="border rounded-lg px-3 py-2 text-white"
             placeholder="••••••••"
           />
         </div>
@@ -30,8 +42,13 @@ function Login(){
           Log in
         </button>
 
-        <p className="text-sm text-center text-blue-600 hover:underline cursor-pointer">
+        <p className="text-sm text-center text-white hover:underline cursor-pointer">
           Forgot Password?
+        </p>
+        <p 
+      onClick={() => navigate("/signup")}
+        className="text-sm text-center text-white hover:underline cursor-pointer">
+          Not Registered?Click Here!
         </p>
       </div>
        </section>

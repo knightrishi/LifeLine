79f427ac65e2503cf7fbf51fdad0fff893351e03
knightrishi <arnav.ara05@gmail.com>
2026-02-13 01:44:59 +0530

    ChartSection Added

---
+++ frontend/Frontend-Life/src/components/Layout/MainGrid/MainGrid.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProfileCard from './ProfileCard'
+import ChartSection from './ChartSection'
+
+const MainGrid = () => {
+  return (
+    <div className='ml-20'>
+      <ProfileCard />
+      <ChartSection />
+    </div>
+  )
+}
+
+export default MainGrid

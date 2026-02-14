5df19376347d34e8b961afbca13a84e003d395b5
knightrishi <arnav.ara05@gmail.com>
2026-02-14 19:44:55 +0530

    Structure Created

---
+++ frontend/Frontend-Life/src/components/Layout/MainGrid/LeaderCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const LeaderCard = () => {
+  return (
+    <div  className="
+        w-[295px]
+        bg-[#213b4e]/80
+        backdrop-blur-2xl
+        h-[500px]
+        border border-white/20
+        mr-10 mt-28 p-6 ml-6
+        rounded-2xl
+        flex flex-col
+        shadow-xl
+      ">
+      
+    </div>
+  )
+}
+
+export default LeaderCard

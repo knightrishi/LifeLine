da8c17216e5453a11d1f55df12492c75939823b3
knightrishi <arnav.ara05@gmail.com>
2026-02-14 19:44:39 +0530

    LeaderCard added

--- frontend/Frontend-Life/src/pages/UserPage.jsx
+++ frontend/Frontend-Life/src/pages/UserPage.jsx
@@ -1,13 +1,34 @@
+import LeaderCard from "../components/Layout/MainGrid/LeaderCard";
+import MainGrid from "../components/Layout/MainGrid/MainGrid";
+import Sidebar from "../components/Layout/Sidebar";
+import TopBar from "../components/Layout/TopBar";
 import UserNav from "../components/UserNav";
-
 
 function UserPage(){
  return(
-    <div>
-       <UserNav />
-    </div>
+<div className="
+  min-h-screen
+  rounded-3xl
+  overflow-hidden
+  grid
+  grid-cols-[auto_1fr_300px]
+">
+  <Sidebar />
 
+  {/* Main Content */}
+  <div className="flex flex-col gap-2 ml-5 mt-1">
+    <TopBar />
+    <MainGrid />
+  </div>
+
+  {/* Right Column */}
+  <div className="pr-6">
+    <LeaderCard />
+  </div>
+
+</div>
  )
 }
+
 
 export default UserPage;

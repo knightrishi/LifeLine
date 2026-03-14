b5fc6c5c560a7fd220bc1f69e8402ccce58c0064
knightrishi <arnav.ara05@gmail.com>
2026-03-15 00:40:07 +0530

    add StandardDefinition model with donation and storage rules

--- server/models/StandardDefinition.js
+++ server/models/StandardDefinition.js
@@ -1,60 +1,82 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const standardSchema = new mongoose.Schema({
+  minAge: {
+    type: Number,
+    default: 18,
+  },
+  maxAge: {
+    type: Number,
+    default: 65,
+  },
+  minWeight: {
+    type: Number,
+    default: 50, // kg
+  },
+  donationVolumeMin: {
+    type: Number,
+    default: 350,
+  },
+  donationVolumeMax: {
+    type: Number,
+    default: 450,
+  },
 
-    minAge: {
-        type: Number,
-        default: 18
-    },
-    maxAge: {
-        type: Number,
-        default: 65
-    },
-    minWeight: {
-        type: Number,
-        default: 50 // kg
-    },
-    donationVolume: {
-        type: String,
-        default: "350-450 ml"
-    },
-    donationFrequency: {
-        type: String,
-        default: "Every 3 months (males) / Every 4 months (females)"
-    },
-    nextEligibleDaysM: {
-        type: Number,
-        default: 90  // days for males
-    },
-    nextEligibleDaysF: {
-        type: Number,
-        default: 120  // days for females
-    },
-    maxDonationsBloodPerYear: {
-        type: Number,
-        default: 4
-    },
-    bloodStorageDuration: {
-        type: String,
-        default: "35-42 days"
-    },
-    plasmaFrequency: {
-        type: String,
-        default: "Every 2-4 weeks"
-    },
-    plasmaStorageDuration: {
-        type: String,
-        default: "Up to 1 year (frozen)"
-    },
-    maxDonationsPlasmaPerYear: {
-        type: String,
-        default: "24-26 times"
-    },
-    canSaveLive: {
-        type: String,
-        default: "1 unit can save up to 3 lives"
-    },
-});
+  nextEligibleDaysM: {
+    type: Number,
+    default: 90, // days for males
+  },
+  nextEligibleDaysF: {
+    type: Number,
+    default: 120, // days for females
+  },
+  maxDonationsBloodPerYear: {
+    type: Number,
+    default: 4,
+  },
+  bloodStorageDuration: {
+    type: Number,
+    default: 35,
+  },
+  plasmaFrequency: {
+    type: Number,
+    default: 21,
+  },
+  plasmaStorageDuration: {
+    type: Number,
+    default: 365,
+  },
+  maxDonationsPlasmaPerYear: {
+    type: Number,
+    default: 24,
+  },
+  plateletsStorageDays: {
+    type: Number,
+    default: 5,
+  },
+  rbcStorageDays: {
+    type: Number,
+    default: 42,
+  },
+  cryoStorageDays: {
+    type: Number,
+    default: 365,
+  },
+  wholeBloodStorageDays: {
+    type: Number,
+    default: 42,
+  },
+  canSaveLive: {
+    type: Number,
+    default: 3,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+},{ timestamps: true });
+
+standardSchema.index({ isActive: 1 }, { unique: true, sparse: true });
 
 const StandardDefinition = mongoose.model("StandardDefinition", standardSchema);
 module.exports = StandardDefinition;

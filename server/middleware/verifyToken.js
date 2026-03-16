ccdd7ff7f6226ed171269ac06938ea33c6d2340a
knightrishi <arnav.ara05@gmail.com>
2026-03-17 02:16:53 +0530

    Token verification done

---
+++ server/middleware/verifyToken.js
@@ -0,0 +1,20 @@
+const verifyToken=(req,res,next)=>{
+
+   const authHeader=req.header.authorization;
+   if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "No token provided" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try{
+        const decoded=jwt.verify(token,process.env.JWT_TOKEN);
+        req.user=decoded;
+        next();
+
+     } catch (error) {
+        return res.status(401).json({ message: "Invalid or expired token" });
+    }
+};
+
+module.exports = verifyToken;

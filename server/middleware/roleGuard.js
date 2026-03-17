d9ca46ae4ac1997892a932a59b82bf2b300a5c98
knightrishi <arnav.ara05@gmail.com>
2026-03-18 02:26:09 +0530

    Access Control added

---
+++ server/middleware/roleGuard.js
@@ -0,0 +1,16 @@
+//const jwt=require('jsonwebtoken');
+
+const roleGuard = (allowedRoles)=>{
+    return (req,res,next)=>{
+            const roles=Array.isArray(allowedRoles) ? allowedRoles : [allowedRoles];
+
+            if(!req.user){
+                return res.status(401).json({ message: "Not authenticated" })
+            }
+            if(!roles.includes(req.user.role)){
+                return res.status(403).json({ message: "Access Denied" })
+            }
+            next();
+    }
+}
+module.exports=roleGuard;

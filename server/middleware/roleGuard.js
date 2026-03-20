40f5b1bcb0a15f9e02a745399ae0be2f78c503b5
knightrishi <arnav.ara05@gmail.com>
2026-03-21 01:51:40 +0530

    ROLE GUARD ADDED

--- server/middleware/roleGuard.js
+++ server/middleware/roleGuard.js
@@ -1,5 +1,5 @@
 //const jwt=require('jsonwebtoken');
-
+//for like who can access what like donor can only acces hi part not hospital employee data or inventory data
 const roleGuard = (allowedRoles)=>{
     return (req,res,next)=>{
             const roles=Array.isArray(allowedRoles) ? allowedRoles : [allowedRoles];

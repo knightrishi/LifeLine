9b30e59bd1ada6d5d5ceccfec3b13b4baf0b8260
knightrishi <arnav.ara05@gmail.com>
2026-03-30 23:36:05 +0530

    Updated db file

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,13 @@
 const mongoose=require("mongoose")
 
 const connectDB =async () =>{
+    if (!process.env.MONGO_URI) {
+        console.error("MONGO_URI not found in .env");
+        process.exit(1);
+    }
 try{
     const conn=await mongoose.connect(process.env.MONGO_URI)
-    console.log("DB CONNECTED");
+    console.log(`DB Connected: ${conn.connection.host}`)
 }catch(error){
     console.error(`Error ${error.message}`);
     process.exit(1);    //for killing the server

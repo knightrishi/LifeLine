c7d3ef670c857749e6b8197e9a08250b5750d533
knightrishi <arnav.ara05@gmail.com>
2026-04-05 17:35:07 +0530

    Crud routes added

--- server/index.js
+++ server/index.js
@@ -9,6 +9,13 @@ const employeeAuthRoutes = require("./routes/auth/employee.auth.js")
 const donorAuthRoutes = require("./routes/auth/donor.auth.js");
 const hospitalAuthRoutes = require("./routes/auth/hospital.auth.js")
 const bloodbankAuthRoutes = require("./routes/auth/bloodbank.auth.js")
+const donorRoutes = require("./routes/donor.routes.js")
+const hospitalRoutes = require("./routes/hospital.routes.js")
+const bloodBankRoutes  = require("./routes/bloodbank.routes.js")
+const employeeRoutes   = require("./routes/employee.routes.js")
+
+
+
 
 connectDB();
 
@@ -28,10 +35,25 @@ app.get("/", (req,res) => {
     })
 })
 
+
+
+// Auth routes
 app.use("/api/auth/donor", donorAuthRoutes);
 app.use("/api/auth/hospital", hospitalAuthRoutes);
 app.use("/api/auth/employee", employeeAuthRoutes)
 app.use("/api/auth/bloodbank", bloodbankAuthRoutes)
+
+
+//CRUD Routes
+app.use("/api/donor", donorRoutes)
+app.use("/api/hospital", hospitalRoutes)
+app.use("/api/bloodbank",  bloodBankRoutes)
+app.use("/api/employee",   employeeRoutes)
+
+
+
+
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(err.statusCode || 500).json({

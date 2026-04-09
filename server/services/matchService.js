295b746190ab31ac8aa2138811f8a27e262ee63b
knightrishi <arnav.ara05@gmail.com>
2026-04-10 00:23:17 +0530

    Match service for finding right donors

--- server/services/matchService.js
+++ server/services/matchService.js
@@ -1,3 +1,36 @@
-const express = require("express");
-const router = express.Router();
+
 const Donor=require('../models/Donor')
+//const Request=require('../models/RequestLog')
+
+async function findMatchingDonors(request){
+    try{
+        const {requiredBloodType, location}=request;
+        
+        if (!location || !location.coordinates) {
+            throw new Error("Invalid request location");
+        }
+
+        const donors=await Donor.find({
+            bloodGroup: requiredBloodType,
+            status: 1,
+            nextEligibleDate: { $lte: new Date() },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: "Point",
+                        coordinates: location.coordinates
+                    },
+                    $maxDistance: 20000
+                }
+            }
+        })
+        .limit(10);
+        
+        return donors;
+
+    }catch(err){
+  console.error("MatchService Error:", err);
+    throw err;
+    }
+}
+module.exports = findMatchingDonors;
